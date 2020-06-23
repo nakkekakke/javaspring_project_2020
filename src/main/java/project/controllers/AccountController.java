40b53bbc0e2335654ac0af3028be8234e0af36d6
nakkekakke <juntusen.niko@gmail.com>
2020-06-24 02:26:02 +0300

    missing import

--- src/main/java/project/controllers/AccountController.java
+++ src/main/java/project/controllers/AccountController.java
@@ -2,6 +2,7 @@ package project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
